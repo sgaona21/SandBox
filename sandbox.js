3e8df7bd69ae1c63fa0c2e7b68429407ca1873e8
sgaona21 <stevegaona21@gmail.com>
2023-03-06 21:20:29 -0600

    node

--- sandbox.js
+++ sandbox.js
@@ -1,16 +1,23 @@
 console.log("js linked")
 
-// Connect to the API URL: https://teamtreehouse.com/profiles/csalgado.json
+const https = require("https");
 
+function getProfile() {
+// Connect to the API URL: https://teamtreehouse.com/profiles/csalgado.json
+const request = https.get("https://teamtreehouse.com/profiles/csalgado.json", 
+(response) => {
+    console.dir(response.statusCode);
+}
+);
 //Read the data:
 
 // Parse the data: 
 
 // Print the data:  
+}
 
-import  { sum }  from './modules.js';
+getProfile();
 
-console.log(sum())
 
 
 
