79e5b671e7888233034fd673ea7b9495568550e1
sgaona21 <stevegaona21@gmail.com>
2023-03-18 14:31:30 -0500

    functions

--- sandbox.js
+++ sandbox.js
@@ -1,33 +1,16 @@
 console.log("JS Linked");
 
-let correctAnswers = 0;
-let crown;
 
-function quizQuestion(city, team) {
-let question = prompt(`Which NBA team is from ${city}?`);
-if (question.toLowerCase() === team.toLowerCase()) {
-  correctAnswers++
-}
+//funciton declaration: 
+function a() {
+    console.log("this is a declaration")
 }
 
-quizQuestion("Phoenix", "Suns");
-quizQuestion("Memphis", "Grizzlies");
-quizQuestion("Miami", "Heat");
-quizQuestion("Charlette", "Hornets");
-quizQuestion("Golden State", "Warriors");
-
-
-if (correctAnswers == 5) {
-  crown = "Gold"
-} else if (correctAnswers == 3 || correctAnswers == 4) {
-  crown = "Silver"
-} else if (correctAnswers == 2 || correctAnswers == 1) {
-  crown = "Bronze"
-} else {
-  crown = "NO CROWN"
+//function expression:
+let b = function() {
+    console.log("this is an expression")
 }
 
-document.querySelector(".myheader").innerHTML = `You got ${correctAnswers} out of 5 questions correct. Crown earned: ${crown}`
-
-
+//arrow function: 
+let c = () => console.log("This is an arrow function");
 
