bfacd4321cb73c887e9bd18a7f3577ea9bb497d1
sgaona21 <stevegaona21@gmail.com>
2023-03-22 23:02:42 -0500

    worked with loops

--- sandbox.js
+++ sandbox.js
@@ -1,23 +1,5 @@
 console.log("JS Linked");
 
-const multiplyUs = (num1, num2) => console.log(num1 * num2); 
-
-
-
-const subtractFromTen = (num) => { 
-	const diff = 10 - num; 
-	console.log(diff);
-};
-
-
-
-const printDate = date => console.log(date);
-
-
-multiplyUs(2,6);
-subtractFromTen(4);
-printDate('1/16/18');
-
-
-
-console.log()
+for ( let j = 0; j <= 100; j += 10 ) {
+    console.log( j );
+  }
