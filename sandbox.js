3928453a527d115751511f1815a416fb26259cab
sgaona21 <stevegaona21@gmail.com>
2023-03-24 16:14:58 -0500

    number guessing game with arrays

--- sandbox.js
+++ sandbox.js
@@ -1,48 +1,25 @@
 console.log("JS Linked");
 
-const arizona = {
-  capital: "Phoenix",
-  population: 1500000,
-  political_party: "Republican",
-  confederate: false,
-  cities: ["Tucson", "Oracle", "Chandler", "Douglas"],
-  governer: "deucey"
-};
-
-const texas = {
-  capital: "Austin",
-  population: 3000000,
-  political_party: "Republican",
-  confederate: true,
-  cities: ["Dallas", "Amarillo", "Houston", "San Antonio"]
-};
-
-const texarona = {
-  ...arizona,
-  ...texas
+function getRandomNumber(upper) {
+  return Math.floor( Math.random() * upper ) + 1;
 }
 
+const answer = getRandomNumber(100);
+// console.log(`Hint: ${answer}`);
+let attempts = 0;
+document.getElementById("header").innerHTML = `5 tries are UP! YOU SUCK!!!!`
 
-const pets = [
-  {name: "sparky", type: "female", breed: "pug", age: 5, photo: "dogs/dog1.jpg"},
-  {name: "dummy", type: "male", breed: "pitbull", age: 1, photo: "dogs/dog2.jpg"},
-  {name: "ugly", type: "female", breed: "lab", age: 7, photo: "dogs/dog3.jpg"},
-  {name: "timmy", type: "male", breed: "chihuahua", age: 4, photo: "dogs/dog4.jpg"},
-  {name: "lexi", type: "female", breed: "doberman", age: 3, photo: "dogs/dog5.jpg"}
-];
-
-let text = '';
-
-function display(array) {
-for (let i = 0; i < array.length; i++) {
-  text += `<h2>${array[i].name}</h2>
-  <h3> ${array[i].type} | ${array[i].breed}</h3>
-  <p>Age: ${array[i].age}</p>
-  <img src="${array[i].photo}" alt="Australian Shepherd">`;
-}
+for (let i = 0; i < 5; i++) {
+  let guess = prompt("I am thinking of a number between 1 and 100. What is it?");
+  attempts++;
+  console.log(`Attempts: ${attempts}`)
+  if (guess > answer) {
+    console.log("Lower");
+  } else {
+    console.log("Higher")
+  }
+  if (guess == answer) {
+    document.getElementById("header").innerHTML = `SUCCESS! It took you ${attempts} tries to guess the number ${answer}! WOW!`;
+    break;
+  };
 };
-
-display(pets);
-
-
-document.getElementById("paragraph").innerHTML = text;
