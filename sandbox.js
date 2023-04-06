ec87822d5eafe6dee4e51f8d62cda6c620400647
sgaona21 <stevegaona21@gmail.com>
2023-04-05 21:15:20 -0500

    querys

--- sandbox.js
+++ sandbox.js
@@ -4,12 +4,11 @@ function randomColor() {
     function getRandomNumber(upper) {
     return Math.floor( Math.random() * upper ) + 1;
   }
-
   let red = getRandomNumber(255);
   let green = getRandomNumber(255);
   let blue = getRandomNumber(255);
+  let textColor = `rgb(${red}, ${green}, ${blue})`;
 
-  let textColor = `rgb(${red}, ${green}, ${blue})`
   return textColor;
 }
 
@@ -28,4 +27,6 @@ button.addEventListener("click", changeRed);
 const moreItems = document.querySelectorAll("li");
 
 console.log(moreItems[2])
+
+
 
