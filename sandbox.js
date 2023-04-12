1a0ce96b2ec97ca78e2801c95f2047869890329b
sgaona21 <stevegaona21@gmail.com>
2023-04-11 21:41:46 -0500

    clear

--- sandbox.js
+++ sandbox.js
@@ -9,19 +9,10 @@ const unorderedList = document.querySelector("ul");
 const listItems = document.getElementsByTagName("li");
 const listItemsArray = [...listItems]
 const addButton = document.getElementById("add-button")
-let deleteButton = '';
 /////VARIABLES/////
 
 
-addButton.addEventListener("click", () => {
-    let newItem = document.createElement("li");
-    newItem.innerHTML = `${userInput.value} <button id="item-delete"> Delete </button>`
-    unorderedList.append(newItem)
 
-    deleteButton = document.getElementById("item-delete")
-
-    userInput.value = ''
-})
 
 
 
