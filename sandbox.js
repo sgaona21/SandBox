d0a4bfe2d0bec881420fd329967ac7104e3fda65
sgaona21 <stevegaona21@gmail.com>
2023-04-12 21:23:15 -0500

    variables

--- sandbox.js
+++ sandbox.js
@@ -11,28 +11,6 @@ const listItemsArray = [...listItems]
 const addButton = document.getElementById("add-button")
 /////VARIABLES/////
 
-userInput.addEventListener('keydown', (event) => {
-    if (event.key === "Enter") {
-    let newListItem  = document.createElement("li");
-    newListItem.innerHTML = userInput.value; 
-    unorderedList.append(newListItem);
-
-    let deleteButton = document.createElement("button");
-    deleteButton.innerHTML = "Delete"; 
-    deleteButton.id = "delete-button"
-    newListItem.append(deleteButton);
-
-    userInput.value = '';
-    }    
-});
-
-
-unorderedList.addEventListener("click", (event) => {
-    if (event.target.id === "delete-button") {
-        console.log(event.target.parentNode)
-        event.target.parentNode.remove();
-    }
-});
 
 
 
