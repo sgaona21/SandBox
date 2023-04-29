c43670d81c76ba0132a97a2ef8a2aa8a180d5c72
sgaona21 <stevegaona21@gmail.com>
2023-04-29 00:44:58 -0500

    leetcode accpted 48ms/beats 95%

--- sandbox.js
+++ sandbox.js
@@ -11,31 +11,23 @@ const listItemsArray = [...listItems]
 const addButton = document.getElementById("add-button")
 /////VARIABLES/////
 
-let needle = 'sad';
-let haystack = 'sjsjsjsjsjshhhahdhhsad';
-// console.log(haystack[9])
-
-/// Find first occurance of needle in haystack. and return index of first letter of the string of needle
-
+let needle = 'issip';
+let haystack = 'mississippi';
 
 var strStr = function(haystack, needle) {
-    let tracker = 0;
+    let tracker=0;
         for (let i = 0; i < haystack.length; i++) {
             if (haystack[i] === needle[tracker]) {
                 tracker++;    
-                console.log(tracker) 
-            } else { 
+            } else if (!haystack[i] === needle[tracker]){ 
                 tracker--;
-                // console.log(tracker)
                 if (tracker < 0) {
                     tracker = 0;
                 }
               }
             if (tracker === needle.length) {
-                console.log()
-                return "Its there!!"
+                return haystack.indexOf(needle)
             }
-            console.log(tracker)
         }
         return -1
 };
