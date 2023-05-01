09782be8673628412ab4300b5a721707cede438b
sgaona21 <stevegaona21@gmail.com>
2023-05-01 01:01:55 -0500

    Update sandbox.js
    leets 5%

--- sandbox.js
+++ sandbox.js
@@ -11,88 +11,57 @@ const listItemsArray = [...listItems]
 const addButton = document.getElementById("add-button")
 /////VARIABLES/////
 
-let stringer = "  who in the world does that  "
-let tester = "hello"
-
-
-
-let reverseArray = []
-let outputString = '';
-console.log(stringer)
-for (let i = stringer.length -1; i <= stringer.length; i--) {
-    console.log(stringer[i])
-    reverseArray.push(stringer[i])
-    if (i === 0) {
-        break
-    }
-}
-for (let a = 0; a < reverseArray.length; a++) {
-    outputString = outputString + reverseArray[a];
-}
-
-let present = 0;
-for (let t = 0; t < reverseArray.length; t++) {
-    if (reverseArray[t] === " ") {
-        reverseArray.shift();
-    }
-    if (reverseArray[t] != " ") {
-        present++
-    }
-    if (present > 0) {
-        break
-    }
-    console.log(present)
-}
-
-let newString = '';
-for (let x = 0; x < outputString.length; x++) {
-    if (outputString[x] != ' ') {
-        newString = newString + outputString[x];
-    }
-}
-
-
-
-
-
-
-
-
-// console.log(removedSpacesString)
-console.log(newString)
-console.log(outputString)
-console.log(reverseArray)
-
-
-
+let s = "  who is that  ";
+let s2 = "a";
+let s3 = "Hello";
+let s4 = "abc";
 
 var lengthOfLastWord = function(s) {
-    
+    console.log(s)
+    ///convert the string to a reversed array///
+    let stringToArray = [];
+    for (let a = s.length -1; a < s.length; a--) {
+        if (a === -1) {
+            break
+        }
+        stringToArray.push(s[a]);
+    }
+    console.log(stringToArray)
+
+    ///remove empty spaces from BEGINNING of array///
+        while (stringToArray[0] === ' ') {
+            stringToArray.shift();
+            if (stringToArray[0] != ' ') {
+            break;
+        }
+    }
+    console.log(stringToArray)
+
+    ///convert the array to string with ONE word///
+    let arrayToString = '';
+    for (let c = 0; c < stringToArray.length; c++) {
+        if (stringToArray[c] === ' ') {
+            break
+        }
+        arrayToString += stringToArray[c];
+    }
+    console.log(arrayToString)
+    console.log("indentation")
+
+    ///reverse the String///
+    let finalString = '';
+    for (let d = arrayToString.length -1; d < arrayToString.length; d--) {
+        if (d === -1) {
+            break
+        }
+        finalString += arrayToString[d];
+    }
+    console.log(finalString)
+
+    return finalString.length;
 };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(lengthOfLastWord(s4))
 
 
 
